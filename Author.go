09323189b9aaf7e950a4a6fb48e4a4bba46460dc
Jacob Martin <jakub.wit.martin@gmail.com>
2016-08-06 15:37:20 +0200

    Organization

--- Author.go
+++ Author.go
@@ -3,6 +3,10 @@ package MeetupRest
 import (
 	"net/http"
 	"github.com/gorilla/mux"
+	"google.golang.org/appengine"
+	"net/url"
+	"google.golang.org/appengine/log"
+	"fmt"
 )
 
 type Author struct {
@@ -25,4 +29,13 @@ func GetAuthorHandler() http.Handler {
 func getAuthorHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
+	params, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		log.Errorf(ctx, "Can't parse query: %v", err)
+		fmt.Fprintf(w, "Can't parse query: %v", err)
+		return
+	}
+	if name, ok := params["name"]; ok == true {
+
+	}
 }
