5aa93b7534890df223f6ee07285eb7972184eb00
Jacob Martin <jakub.wit.martin@gmail.com>
2016-08-21 17:03:15 +0200

    Cleanup.

--- hello.go
+++ hello.go
@@ -1,22 +1,10 @@
 package MeetupRest
 
 import (
-	"context"
-	"fmt"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/gorilla/mux"
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/datastore"
-	"google.golang.org/appengine/log"
 )
-
-type Human struct {
-	Name string
-	Age  int
-}
 
 func init() {
 
@@ -36,27 +24,4 @@ func init() {
 	}
 
 	http.Handle("/", m)
-}
-
-type dsHandler struct {
-}
-
-func (h dsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
-	variables := mux.Vars(r)
-	e := &Human{}
-
-	e.Name = variables["name"]
-	age, _ := strconv.Atoi(variables["age"])
-	e.Age = age
-
-	k := datastore.NewKey(ctx, "People", "", 0, nil)
-	newCtx, _ := context.WithTimeout(ctx, time.Second*2)
-	id, err := datastore.Put(newCtx, k, e)
-	if err != nil {
-		log.Errorf(ctx, "Can't create datastore object: %v", err)
-		return
-	}
-
-	fmt.Fprintf(w, "Your key: %v", id.IntID())
 }
