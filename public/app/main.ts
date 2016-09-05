5692fadf15f24e08a1bdac78590261b5453ab05b
Jacob Martin <jakub.wit.martin@gmail.com>
2016-09-05 22:53:23 +0200

    Angular

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,4 +1,11 @@
+import 'reflect-metadata'
+import 'zone.js/dist/zone'
+import 'core-js'
+
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
